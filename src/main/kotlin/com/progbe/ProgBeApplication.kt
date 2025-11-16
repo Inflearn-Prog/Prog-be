b15435c5b4a3be4884016da28d334dd50ab3aa5e
HyeonSeong <gus8131@naver.com>
2025-11-16 21:37:50 +0900

    first commit

---
+++ src/main/kotlin/com/progbe/ProgBeApplication.kt
@@ -0,0 +1,11 @@
+package com.progbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProgBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProgBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/progbe/ProgBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.progbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProgBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
