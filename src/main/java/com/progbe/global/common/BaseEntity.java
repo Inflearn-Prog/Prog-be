79fdc94674eae39835d970846c74b279dbd7fdf6
kimjunghwannn <aa2002dcks@naver.com>
2026-01-13 16:09:44 +0900

    fit: BaseEntity 추가

---
+++ src/main/java/com/progbe/global/common/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.progbe.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    public void delete() {
+        deletedAt = LocalDateTime.now();
+    }
+
+    @PrePersist
+    public void prePersist() {
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now().withNano(0);
+        }
+    }
+}
