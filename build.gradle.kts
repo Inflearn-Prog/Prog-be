c10e7d5b7d8da8ec1efaf322baaa03284ed79c84
HyeonSeong <gus8131@naver.com>
2026-01-29 17:58:26 +0900

    [hotfix] 1. yml 파일 위치이동 2. propertise 삭제 3. hibernate 의존성 변경 4. test 환경 yml 구성 5. UserSocialLinkEntity mappedBy 오류 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
     // Database
-    developmentOnly("com.h2database:h2")
+    implementation("com.h2database:h2")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.mysql:mysql-connector-j")
 }
