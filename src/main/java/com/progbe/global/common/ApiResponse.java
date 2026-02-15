cbf1e1659ab678c6aed95a84a83dec316a9124fe
Sirius_Alpha <65502248+siria22@users.noreply.github.com>
2026-02-15 15:29:50 +0900

    Fix - 공통 응답 구조 수정 (#24)
    - ApiResponse = Common response 수정
    - Json에서 Null 필드 무시

--- src/main/java/com/progbe/global/common/ApiResponse.java
+++ src/main/java/com/progbe/global/common/ApiResponse.java
@@ -1,38 +1,59 @@
 package com.progbe.global.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
 
-    private final String statusCode;
+    private final boolean success;
+    private final String code;
     private final T data;
     private final ErrorBody error;
     private final LocalDateTime timestamp;
 
-    private ApiResponse(String statusCode, T data, ErrorBody error) {
-        this.statusCode = statusCode;
+    private ApiResponse(boolean success, String code, T data, ErrorBody error) {
+        this.success = success;
+        this.code = code;
         this.data = data;
         this.error = error;
         this.timestamp = LocalDateTime.now();
     }
 
+    // =========== 성공 응답 ===========
     public static <T> ApiResponse<T> success(T data, String code) {
-        return new ApiResponse<>(code, data, null);
+        return new ApiResponse<>(
+                true,
+                code,
+                data,
+                null
+        );
     }
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>("200", data, null); // Default success code =  200
+        return new ApiResponse<>(
+                true,
+                String.valueOf(HttpStatus.OK.value()),
+                data,
+                null
+        );
     }
 
-
+    // =========== 실패 응답 ===========
     public static <T> ApiResponse<T> fail(String httpStatusCode, String errorName, String message) {
-        return new ApiResponse<>(httpStatusCode, null, new ErrorBody(errorName, message));
+        return new ApiResponse<>(
+                false,
+                httpStatusCode,
+                null,
+                new ErrorBody(errorName, message)
+        );
     }
 
-    public record ErrorBody(String code, String message) {
+    public record ErrorBody(String errorClassName, String message) {
         /* nop */
     }
 }
