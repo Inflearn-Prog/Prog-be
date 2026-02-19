f3b82cbfd69d41bd6c2e3536da1b6fa92e21f97a
HyeonSeong <gus8131@naver.com>
2026-02-19 15:36:16 +0900

    feat : 프롬프트 최신순 API 생성

--- src/main/java/com/progbe/domain/prompt/repository/PromptRepository.java
+++ src/main/java/com/progbe/domain/prompt/repository/PromptRepository.java
@@ -29,5 +29,11 @@ public interface PromptRepository extends JpaRepository<PromptEntity, Long> {
             "WHERE p.user.id IN :userIds AND p.deletedAt IS NULL " +
             "GROUP BY p.user.id")
     List<UserCountDto> countByUserIdsGrouped(@Param("userIds") List<Long> userIds);
+
+    @Query("SELECT p FROM PromptEntity p " +
+            "JOIN FETCH p.category " +
+            "WHERE p.category = 'PUBLIC' " +
+            "ORDER BY p.createdAt DESC")
+    Page<PromptEntity> findPromptSortedTime(Pageable pageable);
 }
 
