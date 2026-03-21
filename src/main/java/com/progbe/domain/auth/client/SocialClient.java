7b0318b07b626d961892a547d4de6ecd425c1a1f
Sirius_Alpha <scgv2222@naver.com>
2026-03-21 14:43:37 +0900

    Fix - 로그인 로직 수정
    - 프론트엔드 NextAuth v5 환경 대응을 위해 소셜 로그인 API 구조 변경
    - 백엔드에서의 소셜 토큰 교환 로직 제거

--- src/main/java/com/progbe/domain/auth/client/SocialClient.java
+++ src/main/java/com/progbe/domain/auth/client/SocialClient.java
@@ -1,11 +1,9 @@
 package com.progbe.domain.auth.client;
 
-import com.progbe.domain.user.dto.SocialTokens;
 import com.progbe.global.oauth.OAuth2Attributes;
 
 public interface SocialClient {
     boolean supports(String provider);
-    SocialTokens getSocialTokens(String authCode);
     String refreshAccessToken(String refreshToken);
     OAuth2Attributes getSocialUserInfo(String accessToken);
     void unlink(String accessToken);
