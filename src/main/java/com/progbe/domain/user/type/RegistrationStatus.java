107091b8afe15700abc1862d90378b19decec30f
Sirius_Alpha <65502248+siria22@users.noreply.github.com>
2026-04-06 21:11:48 +0900

    Fix - 로그인 플로우 수정 (2) (#45)
    - 닉네임 등록 완료, 커리어 정보 입력 완료 단계 도입 및 추가
    
    Co-authored-by: Sirius_Alpha <scgv2222@naver.com>

--- src/main/java/com/progbe/domain/user/type/RegistrationStatus.java
+++ src/main/java/com/progbe/domain/user/type/RegistrationStatus.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum RegistrationStatus {
     SOCIAL_LOGIN_ONLY("소셜 로그인만 완료"),
     TERMS_AGREED("약관 동의 완료"),
+    NICKNAME_REGISTERED("닉네임 등록 완료"),
+    CAREER_INFO_COMPLETED("커리어 정보 입력 완료"),
     ONBOARDING_COMPLETED("온보딩 완료");
 
     private final String description;
