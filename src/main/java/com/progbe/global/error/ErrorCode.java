315557b53b834e8955fc98aee1835ec3ccdbb156
hignandlight <junhyunghl126@gmail.com>
2026-04-17 16:49:16 +0900

    Revert "fix : 직무(JobRole) 계층 구조 변경 및 사용자 매핑 구현"
    This reverts commit 2e61e959cd860ef8c5521fb2854ce097ca044d79.

--- src/main/java/com/progbe/global/error/ErrorCode.java
+++ src/main/java/com/progbe/global/error/ErrorCode.java
@@ -56,11 +56,6 @@ public enum ErrorCode {
     FUTURE_DATE_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "미래 날짜는 조회할 수 없습니다."),
     DATE_RANGE_EXCEEDED(HttpStatus.BAD_REQUEST, "조회 가능한 최대 기간은 30일입니다."),
 
-    JOB_ROLE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 직무입니다."),
-    JOB_ROLE_NOT_SELECTABLE(HttpStatus.BAD_REQUEST, "선택 가능한 직무가 아닙니다."),
-    JOB_ROLE_HAS_CHILDREN(HttpStatus.BAD_REQUEST, "하위 직무가 존재하여 삭제할 수 없습니다."),
-    JOB_ROLE_CIRCULAR_REFERENCE(HttpStatus.BAD_REQUEST, "순환 참조가 발생하는 계층 이동은 불가능합니다."),
-
     INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "유효하지 않은 카테고리 정보가 포함되어 있습니다."),
     CATEGORY_NOT_EMPTY(HttpStatus.INTERNAL_SERVER_ERROR, "게시글이 존재하거나 하위 카테고리가 있는 경우 삭제할 수 없습니다."),
     CATEGORY_NAME_DUPLICATE(HttpStatus.BAD_REQUEST, "이미 존재하는 카테고리 이름입니다."),
