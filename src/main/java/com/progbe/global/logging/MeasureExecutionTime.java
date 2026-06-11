ae44e1c81baf5f1607c279ad1eb1cdb3bee2e273
hignandlight <junhyunghl126@gmail.com>
2026-06-11 09:35:45 +0900

    PERF : 메서드 실행시간 측정 AOP 추가

---
+++ src/main/java/com/progbe/global/logging/MeasureExecutionTime.java
@@ -0,0 +1,13 @@
+package com.progbe.global.logging;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MeasureExecutionTime {
+
+    long warnThresholdMs() default 300; //ms 단위 임계값. 초과 시 WARN 레벨로 출력되도록
+}
